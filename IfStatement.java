077549f569c002fa277b9a15d30959618f8c6a45
Ayden Ostoich <ayden.ostoich@MrSquidward.local>
2018-10-04 09:11:26 -0400

    added IfStatement.java

---
+++ IfStatement.java
@@ -0,0 +1,11 @@
+ class IfStatement {
+	public static void main(String args []){
+		int test = 10;
+		if (test = 9){
+			System.out.println("thats a lot of damage");
+		}else{
+		System.out.println("jeff");
+		
+		}
+	}
+}
